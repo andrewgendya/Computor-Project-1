583c9c7b216ddf13ec6ed98d48a1d89f443f054f
Lokaghna <lokaghna.velugub@ufl.edu>
2025-02-13 12:34:10 -0500

    Updated simspon function implementaiton to riemann.py.

---
+++ barycentric.py
@@ -0,0 +1,8 @@
+# get_baryenctric_coordinates implementation - Andrew
+def get_barycentric_coordinates:
+
+# get_Caretsian_coordinates implementation - Devadarshini
+def get_cartesian_coordinates:
+
+#
+def is_inside_triangle:
