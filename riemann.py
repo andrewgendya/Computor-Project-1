38435f55bd54c333c25df66456accefdf554662b
darsh <dhandapani.d@ufl.edu>
2025-02-13 12:22:09 -0500

    Added trapezoidal function

--- riemann.py
+++ riemann.py
@@ -1,6 +1,6 @@
 # simpson function implementation - Lokaghna
 import numpy as np
-def trapezoidal(x_vals: np.ndarray, func: np.ufunc)
+def trapezoidal(x_vals: np.ndarray, func: np.ufunc):
     print("Hello")
 
 def simpson(x_vals: np.ndarray, func: np.ufunc):
