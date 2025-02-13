de188180cf30b7b1ae80113f236210aeb2df1e03
Andrew Gendy <andrewgendyawad@gmail.com>
2025-02-13 12:33:09 -0500

    left endpoint function

--- riemann.py
+++ riemann.py
@@ -6,3 +6,6 @@ def trapezoidal(x_vals: np.ndarray, func: np.ufunc):
 def simpson(x_vals: np.ndarray, func: np.ufunc):
     print("Hello")
 
+def left_endpoint(x_vals: np.ndarray, func: np.ufunc):
+    dx: np.diff(x_vals)
+    return np.sum(func(x_vals[:-1]*dx))
