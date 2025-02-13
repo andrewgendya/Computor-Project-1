917228fa7a50aa9649116aeb6c0ee7f7ab2099ef
Lokaghna <lokaghna.velugub@ufl.edu>
2025-02-13 12:27:46 -0500

    Updated simspon function implementaiton to riemann.py.

--- riemann.py
+++ riemann.py
@@ -1,4 +1,13 @@
+
+
+
+
+
+
 # simpson function implementation - Lokaghna
 import numpy as np
 def simpson(x_vals: np.ndarray, func: np.ufunc):
-    print("Hello")
+    a = x_vals[0]
+    b = x_vals[-1]
+    approx = ((b-a)/6)(func(a)+4*func((a+b)/2)+func(b))
+    return approx
