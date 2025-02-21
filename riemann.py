8914395adce69a7322135cfff90ef3362186bb35
Lokaghna <lokaghna.velugub@ufl.edu>
2025-02-21 16:58:15 -0500

    Updated is_inside_traingle function. Debugged functions in riemann and barycentric. Added "plt.show()" in main notebook to fix testing code not showing output.

--- riemann.py
+++ riemann.py
@@ -1,12 +1,14 @@
 import numpy as np
 # left_endpoint - Andrew Gendy
 def left_endpoint(x_vals: np.ndarray, func: np.ufunc):
-    dx: np.diff(x_vals)
-    return np.sum(func(x_vals[:-1]*dx))
+    dx = np.diff(x_vals)
+    # changed dx: to dx =
+    return np.sum(func(x_vals[:-1])*dx)
+    # moved "*dx" outside func call
 
 # simpson function implementation - Devadarshini
 def trapezoid(x_vals: np.ndarray, func: np.ufunc)->float:
-    integral=np.diff(x_vals)
+    integral = np.diff(x_vals)
     return np.sum(((func(x_vals[:-1]))+(func(x_vals[1:])))/2*integral)
 
 
@@ -15,8 +17,5 @@ def simpson(x_vals: np.ndarray, func: np.ufunc):
     a = x_vals[0]
     b = x_vals[-1]
 
-    approx = ((b-a)/6)(func(a)+4*func((a+b)/2)+func(b))
-    np.diff(x_vals)/6 * np.sum(x_vals)
+    approx = ((b-a)/6)*(func(a)+4*func((a+b)/2)+func(b))
     return approx
-
-print(simpson(np.array([[1,2],[2,1]])), np.sin)
