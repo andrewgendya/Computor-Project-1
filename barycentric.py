18b98122aac0283c7157b851601967032dbb338c
Andrew Gendy <andrewgendyawad@gmail.com>
2025-02-20 19:52:43 -0500

    left endpoint function

--- barycentric.py
+++ barycentric.py
@@ -1,6 +1,16 @@
 # get_baryenctric_coordinates implementation - Andrew
-def get_barycentric_coordinates:
+import numpy as np
 
+
+def get_barycentric_coordinates(triangle_coordinates, point_coordinates):
+    x1,x2,x3=triangle_coordinates[0]
+    y1,y2,y3=triangle_coordinates[1]
+    x,y=point_coordinates
+    denominator=(x2-x1)*(y2-y1)-(y3-y1)*(y2-y1)
+    run1=((x2-x)*(y3-y)-(x3-x)*(y2-y))/denominator
+    run2=((x3-x)*(y1-y)-(x1-x)*(y3-y))/denominator
+    run3=1-run1-run2
+    return np.array([run1,run2,run3])
 # get_Caretsian_coordinates implementation - Devadarshini
 def get_cartesian_coordinates:
 
